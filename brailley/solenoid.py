3a2807101506b0cb1692eed6882107b74dfefa63
Vinícius Couto Tasso <vcoutasso@gmail.com>
2023-05-20 21:51:59 -0300

    chore: add pylint as dev dependency

--- brailley/solenoid.py
+++ brailley/solenoid.py
@@ -2,11 +2,11 @@ import gpiozero
 
 
 class Solenoid:
-    gpio = None
-
     def __init__(self, gpio: int):
         self.gpio = gpio
-        self._output_device = gpiozero.OutputDevice(gpio, pin_factory=gpiozero.pins.pigpio.PiGPIOFactory())
+        self._output_device = gpiozero.OutputDevice(
+            gpio, pin_factory=gpiozero.pins.pigpio.PiGPIOFactory()
+        )
 
     def __repr__(self):
         return f"Solenoid({self.gpio})"
