368f5e77a29f01e27bc2e4174660d411b9b193af
Vinícius Couto Tasso <vcoutasso@gmail.com>
2023-05-21 21:51:12 -0300

    fix: binary filter would crash if applyed to grayscale image

---
+++ brailley/reader/image_reader/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+from .multiple_filters_decorator import MultipleFiltersDecorator
+from .noise_removal_filter import NoiseRemovalFilter
+from .grayscale_filter import GrayscaleFilter
+from .binary_filter import BinaryFilter
+from .image_filter import ImageFilter
--- brailley/reader/image_reader/preprocessing/binary_filter.py
+++ brailley/reader/image_reader/preprocessing/binary_filter.py
@@ -5,7 +5,10 @@ import cv2
 
 class BinaryFilter(ImageFilter):
     def apply_filter(self, array: np.ndarray) -> np.ndarray:
-        grayscale_array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+        try:
+            grayscale_array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+        except Exception:
+            grayscale_array = array
         _, binary_array = cv2.threshold(
             grayscale_array, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU
         )
