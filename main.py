31aba58c8e484242ecadadef17658ad1d4dadf9f
Vinícius Couto Tasso <vcoutasso@gmail.com>
2023-05-22 22:58:47 -0300

    feat: run brailley on button press

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from brailley import Brailley
-
+from gpiozero import Button
+from signal import pause
 
 if __name__ == "__main__":
     brailley = Brailley()
+    button = Button(2)
 
-    brailley.capture_and_display()
+    button.when_pressed = brailley.capture_and_display
+
+    pause()
